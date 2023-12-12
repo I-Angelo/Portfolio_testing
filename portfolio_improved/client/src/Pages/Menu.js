ae6fd2e8036553fc489c45db60b5b94fe59a3df3
I-Angelo <111244396+I-Angelo@users.noreply.github.com>
2023-12-11 22:57:07 -0500

    Images in welcome page

--- portfolio_improved/client/src/Pages/Menu.js
+++ portfolio_improved/client/src/Pages/Menu.js
@@ -1,38 +1,37 @@
-//Menu.js
-
-import React, { useState } from 'react';
+// Menu.js
+import React from 'react';
 import '../Styles/Menu.css'; // Import your CSS file for styling
 import LoginToggle from '../layouts/LoginToggle';
 import GreetingMessage from '../layouts/GreetMessage';
 import MenuToggle from '../layouts/MenuToggle';
-
-
-
-
-
+import IsometricImages from '../layouts/IsometricImages';
+import image1 from "../static/Image1.png"; 
 
 const Menu = () => {
-
-
-  const [menuOpen, setMenuOpen] = useState(false);
-  
-
   return (
     <>
-    {/* // <motion.div className="menu-mobile" initial={exitAnimation} animate={enterAnimation} exit={exitAnimation}> */}
-    
-    <GreetingMessage />
-
-    <LoginToggle />
-
-    <MenuToggle />
-
-    {/* // </motion.div> */}
+      <GreetingMessage />
+      <IsometricImages>
+        <div
+          className="image1"
+          style={{ backgroundImage: `url(${image1})`, backgroundSize: 'cover' }} />
+        <div
+          className="image2"
+          style={{ backgroundImage: 'url(path_to_your_image_2)' }}
+        />
+        <div
+          className="image3"
+          style={{ backgroundImage: 'url(path_to_your_image_3)' }}
+        />
+        <div
+          className="image4"
+          style={{ backgroundImage: 'url(path_to_your_image_4)' }}
+        />
+      </IsometricImages>
+      <LoginToggle />
+      <MenuToggle />
     </>
-  )
-}
+  );
+};
+
 export default Menu;
-
-
-
-
